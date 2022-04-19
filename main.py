990923d2339ac00c54c1b8ab8f0d3a354c085b0a
Renan Lemes Leepkaln <renan_ll@hotmail.com>
2022-04-19 16:14:47 -0300

    CHORES:add new web

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from webScraping import webScraping
+""" 
+    url1 = https://sistemaswebb3-listados.b3.com.br/indexPage/day/IBOV?language=pt-br
+    url2 = https://sistemaswebb3-listados.b3.com.br/indexPage/theorical/IBOV?language=pt-br
+    url3 = https://sistemaswebb3-listados.b3.com.br/indexPage/preview/IBOV?language=pt-br
+ """
+url1 = 'https://sistemaswebb3-listados.b3.com.br/indexPage/day/IBOV?language=pt-br'
+url2 = 'https://sistemaswebb3-listados.b3.com.br/indexPage/theorical/IBOV?language=pt-br'
+url3 = 'https://sistemaswebb3-listados.b3.com.br/indexPage/preview/IBOV?language=pt-br'
 
-df = webScraping(
-    url="https://sistemaswebb3-listados.b3.com.br/indexPage/day/IBOV?language=pt-br")
 
+lista1 = webScraping(url=url1)
 
-print(df.meth())
+lista2 = webScraping(url=url2)
+
+lista3 = webScraping(url=url3)
